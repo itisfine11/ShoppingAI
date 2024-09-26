296c2cb721a830199ccd4e28b68abfe40c5e7aa2
itisfine11 <realdd521@gmail.com>
2024-09-26 15:49:25 +0700

    update

--- shoppinggpt/config.py
+++ shoppinggpt/config.py
@@ -15,4 +15,6 @@ DATA_TEXT_PATH = r"E:\chatbot\ShoppingGPT\data\policy.txt"
 STORE_DIRECTORY = r"E:\chatbot\ShoppingGPT\data\datastore"
 
 # Embeddings
+load_dotenv(r"E:\chatbot\ShoppingGPT\.env")
+GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
 EMBEDDINGS = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
